a47c9b9ff9b608a80b570bb2d95d97a4e2369aad
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-10-11 16:38:25 +0600

    custom middleware  added

--- routes/superAdmin.php
+++ routes/superAdmin.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Super Admin route
-// Route::group(['prefix' => 'super-admin/', 'namespace' => 'SuperAdmin', 'as' => 'superAdmin.', 'middleware' => ['auth', 'superAdmin']], function () {
-Route::group(['prefix' => 'super-admin/', 'namespace' => 'SuperAdmin', 'as' => 'superAdmin.'] , function () {
+Route::group(['prefix' => 'super-admin/', 'namespace' => 'SuperAdmin', 'as' => 'superAdmin.', 'middleware' => ['auth', 'superAdmin']], function () {
     Route::get('/home', function () {
             return view('SuperAdmin.dashboard');
     });
