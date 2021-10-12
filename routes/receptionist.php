add46c0969f443c62735ef417f12dfec253cbb5b
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-10-12 10:44:49 +0600

    Dashboard redirected

--- routes/receptionist.php
+++ routes/receptionist.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 // Receptionist route
 Route::group(['prefix' => 'receptionist/', 'namespace' => 'Receptionist', 'as' => 'receptionist.', 'middleware' => ['auth', 'receptionist']], function () {
 
+    Route::get('dashboard', 'DashboardController@dashboard')->name('dashboard');
+
 });
