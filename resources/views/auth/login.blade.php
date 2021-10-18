087cc1e4b8ca3fbb063a3ceedd233815c2a30848
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-10-18 13:35:15 +0600

    login not completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,133 @@
-@extends('layouts.app')
+@extends('auth.master')
+
+@push('title')
+    Log In
+@endpush
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/center-part/css/Administrator-3.css') }}">
+@endpush
+@section('content')
+    <div class="container my-5 ">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12 mb-5">
+                <!-- ==============card header title start=============== -->
+                <div class="card administrator-card-head w-75 mx-auto">
+                    <div class="card-body  text-center">
+                        <h6 class="">Log in</h6>
+                    </div>
+                </div>
+                <!-- =================card header title start=============== -->
+                <!-- =================card form start======================== -->
+                <div class="card w-75 shadow mx-auto">
+                    <div class="card-body">
+                        <form action="javascript:void(0)">
+                            <div class="mb-3">
+                                <label for="phone" class="form-label text-muted">Phone Number</label>
+                                <input name="phone" id="phone" type="text" class="form-control @error('email') is-invalid @enderror" placeholder='Enter Phone Number'>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label text-muted">Password</label>
+                                <input type="password" class="form-control @error('email') is-invalid @enderror" id="password">
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="addministator-form-sub-btn">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!--=================== card form end =========================-->
+            </div>
+            <!--================== Otp right side div start==================== -->
+            <div class="col-lg-6 col-md-12  col-sm-12 text-center mt-3 otp-reg">
+                <div class="right-side-form">
+                    <p class="text-muted">An OTP is just sent to your mobile <br> <span class="otp-sent-number"></span></p>
+                    <p class="text-muted">Enter the OTP here :</p>
+                    <!--======================== otp card start here==================== -->
+                    <div class="d-flex justify-content-center">
+                        <div class="card shadow w-50">
+                            <div class="card-body">
+                                <h5 class="card-title text-muted">OTP verification</h5>
+                                <div class='text-center otp-card-place my-2'>
+                                    <input type="text" class='form-control otp-card-place' placeholder='12345' />
+                                </div>
+                                <a href="#" class="card-link"><button class='btn text-muted'>Resend</button></a>
+                                <a href="#" class="card-link"><button
+                                        class=' otp-btn-verify text-light'>Verify</button></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!--======================== otp card end here==================== -->
+                </div>
+                <button class="mt-5  apply-reg-btn">Administrator Login</button>
+            </div>
+            <!--================== Otp right side div end==================== -->
+        </div>
+    </div>
+@endsection
+
+@push('script')
+    <script>
+        $(document).ready(function() {
+            $('.otp-reg').hide();
+            $('.addministator-form-sub-btn').click(function() {
+                var formData = new FormData();
+                formData.append('phone', $('#phone').val());
+                formData.append('password', $('#password').val());
+                $.ajax({
+                    method: 'POST',
+                    url: '/login',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function(data) {
+                        // $('#main-form').trigger("reset");
+                        console.log(data.message);
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: data.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+
+                        $('.otp-reg').show();
+                        $('.otp-sent-number').text($('#phone').val());
+                        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        // setTimeout(function() {
+                        //     location.reload();
+                        // }, 800); //
+                    },
+                    error: function(xhr) {
+                        var errorMessage = '<div class="card bg-danger">\n' +
+                            '                        <div class="card-body text-center p-5">\n' +
+                            '                            <span class="text-white">';
+                        $.each(xhr.responseJSON.errors, function(key, value) {
+                            errorMessage += ('' + value + '<br>');
+                        });
+                        errorMessage += '</span>\n' +
+                            '                        </div>\n' +
+                            '                    </div>';
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            footer: errorMessage
+                        })
+                    },
+                })
+            });
+        });
+    </script>
+@endpush
+
+
+
+
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +199,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
