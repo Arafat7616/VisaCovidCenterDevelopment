e6ad9bccd212c54e0dfd11e790c4ac7342a50eb6
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-10-26 16:40:03 +0600

    css & js file divided for calender

--- resources/views/Administrator/regularManPower/index.blade.php
+++ resources/views/Administrator/regularManPower/index.blade.php
@@ -6,18 +6,18 @@
 
 @push('css')
     <link rel="stylesheet" href="{{ asset('assets/center-part/css/calender_14.css') }}">
+   <link rel="stylesheet" href="{{ asset('assets/center-part/css/third-party-calender/calender.css') }}">
 @endpush
 
 @section('content')
-<div class="container container-bg-color my-3 ">
-    <div class="card-body shadow " style="margin-bottom: 120px;">
-        <div class="container">
-            <div class="row">
-                <form action="">
-                    <h1 class="cal-header">Manpwoer Schedule</h1>
-                    <div class="cal-body ">
+    <div class="container container-bg-color my-3 ">
+        <div class="card-body shadow " style="margin-bottom: 120px;">
+            <div class="container">
+                <div class="row">
+                        <h1 class="cal-header">Manpwoer Schedule</h1>
+                        <div class="cal-body ">
 
-                        <div class="month">
+                            {{-- <div class="month">
                             August 2021
                             <button class="cal-left-l"><i class="fas fa-chevron-left"></i></button>
                             <button class="cal-right-r"><i class="fas fa-chevron-right"></i></button>
@@ -173,122 +173,140 @@
                                     </button> </td>
                             </tr>
 
-                        </table>
-                    </div>
-                    <div class="cal-slot row ">
-                        <div class="cal-morning-slot">
-                            <p class="morning">Morning Slot</p>
-                            <div class="cal-morn-in">
-                                <input type="time" class="cal-morn-in-left" placeholder="9:45 AM"> -
-                                <input type="time" class="cal-morn-in-left" placeholder="12:00 PM">
-                            </div>
-                            <p class="cal-footer">Working period: 545 Minutes</p>
-                        </div>
+                        </table> --}}
 
-                        <div class="cal-day-slot">
-                            <p class="day">Day Slot</p>
-                            <div class="cal-day-in">
-                                <input type="time" class="cal-morn-day-right" placeholder="9:45 AM"> -
-                                <input type="time" class="cal-morn-day-right" placeholder="12:00 PM">
-                            </div>
-                            <p class="cal-footer">Working period: 135 Minutes</p>
 
+
+                            <div class="calendar calendar-first p-5" id="calendar_first">
+                                <div class="calendar_header">
+                                    <button class="switch-month switch-left"> <i class="fa fa-chevron-left"></i></button>
+                                    <h2></h2>
+                                    <button class="switch-month switch-right"> <i class="fa fa-chevron-right"></i></button>
+                                </div>
+                                <div class="calendar_weekdays"></div>
+                                <div class="calendar_content"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="cal-min-slot row">
-                        <ul class="total">
-                            <li class="cal-min-l">Total minutes</li>
-                            <li class="cal-min-r">815</li>
-                        </ul>
-                        <ul class="total">
-                            <li class="cal-min-l">Total volunteers on center</li>
-                            <li class="cal-min-r"><input type="text" class="cal-min-t" placeholder="5"></li>
-                        </ul>
-                        <ul class="total">
-                            <li class="cal-min-l">Total man minutes per day</li>
-                            <li class="cal-min-r">4075</li>
-                        </ul>
-                    </div>
-                    <div class="cal-service-slot row">
-                        <table class="t-cal">
-                            <tr class="cal-mx-x">
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> <b>Service</b> </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> <small>Estimated time</small><br><b>Per Process</b> </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> <small>Max service</small><br><b>Per day</b> </p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> PCR </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mxx">20 <small class="s-cx">min</small></p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> 204 </p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> Vaccine </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mxx"> 15 <small class="s-cx">min</small></p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> 272 </p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> Booster </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mxx"> 15 <small class="s-cx">min</small> </p>
-                                </td>
-                                <td class="cal-x-y">
-                                    <p class="p-mx"> 272 </p>
-                                </td>
-                            </tr>
-                            <tr class="cal-mx-x-i">
-                                <td></td>
-                                <td>
-                                    <p class="p-mx">Average service per day </p>
-                                </td>
-                                <td>
-                                    <p class="p-mx">249 </p>
-                                </td>
-                            </tr>
-                            <tr class="cal-mx-x-p">
-                                <td></td>
-                                <td>
-                                    <p class="p-mx">Want to service per day </p>
-                                </td>
-                                <td>
-                                    <p class="p-mx y-s">230 </p>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="check-cal">
-                        <input type="checkbox" checked="checked" value="Bike" class="check-x"> Set this as default
-                        for everyday<br>
-                    </div>
-                    <div class="cal-save">
-                        <button class="cal-x-btn">Save</button>
-                    </div>
-                </form>
+                    <form action="">
+
+                        <div class="cal-slot row ">
+                            <div class="cal-morning-slot">
+                                <p class="morning">Morning Slot</p>
+                                <div class="cal-morn-in">
+                                    <input type="time" class="cal-morn-in-left" placeholder="9:45 AM"> -
+                                    <input type="time" class="cal-morn-in-left" placeholder="12:00 PM">
+                                </div>
+                                <p class="cal-footer">Working period: 545 Minutes</p>
+                            </div>
+
+                            <div class="cal-day-slot">
+                                <p class="day">Day Slot</p>
+                                <div class="cal-day-in">
+                                    <input type="time" class="cal-morn-day-right" placeholder="9:45 AM"> -
+                                    <input type="time" class="cal-morn-day-right" placeholder="12:00 PM">
+                                </div>
+                                <p class="cal-footer">Working period: 135 Minutes</p>
+
+                            </div>
+                        </div>
+                        <div class="cal-min-slot row">
+                            <ul class="total">
+                                <li class="cal-min-l">Total minutes</li>
+                                <li class="cal-min-r">815</li>
+                            </ul>
+                            <ul class="total">
+                                <li class="cal-min-l">Total volunteers on center</li>
+                                <li class="cal-min-r"><input type="text" class="cal-min-t" placeholder="5"></li>
+                            </ul>
+                            <ul class="total">
+                                <li class="cal-min-l">Total man minutes per day</li>
+                                <li class="cal-min-r">4075</li>
+                            </ul>
+                        </div>
+                        <div class="cal-service-slot row">
+                            <table class="t-cal">
+                                <tr class="cal-mx-x">
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> <b>Service</b> </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> <small>Estimated time</small><br><b>Per Process</b> </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> <small>Max service</small><br><b>Per day</b> </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> PCR </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mxx">20 <small class="s-cx">min</small></p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> 204 </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> Vaccine </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mxx"> 15 <small class="s-cx">min</small></p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> 272 </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> Booster </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mxx"> 15 <small class="s-cx">min</small> </p>
+                                    </td>
+                                    <td class="cal-x-y">
+                                        <p class="p-mx"> 272 </p>
+                                    </td>
+                                </tr>
+                                <tr class="cal-mx-x-i">
+                                    <td></td>
+                                    <td>
+                                        <p class="p-mx">Average service per day </p>
+                                    </td>
+                                    <td>
+                                        <p class="p-mx">249 </p>
+                                    </td>
+                                </tr>
+                                <tr class="cal-mx-x-p">
+                                    <td></td>
+                                    <td>
+                                        <p class="p-mx">Want to service per day </p>
+                                    </td>
+                                    <td>
+                                        <p class="p-mx y-s">230 </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="check-cal">
+                            <input type="checkbox" checked="checked" value="Bike" class="check-x"> Set this as
+                            default
+                            for everyday<br>
+                        </div>
+                        <div class="cal-save">
+                            <button class="cal-x-btn">Save</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push('script')
+    <script src="{{ asset('assets/center-part/js/third-party-calender/calender.js') }}"></script>
+    <script>
 
+    </script>
 @endpush
