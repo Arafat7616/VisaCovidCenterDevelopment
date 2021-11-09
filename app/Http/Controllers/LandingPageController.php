365c031349bf00bad00e29b905346c066553b6d5
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-11-09 16:58:23 +0600

    subscription complete from landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,15 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use App\Models\WebsiteService;
 use App\Models\WebsiteWork;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $services = WebsiteService::all();
         $weWorks = WebsiteWork::all();
-        return view('welcome', compact('services','weWorks'));
+        return view('welcome', compact('services', 'weWorks'));
+    }
+
+    // subscribe Store
+    public function subscribeStore(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
+        if (Subscriber::where('email', $request->email)->exists()) {
+            return response()->json([
+                'type' => 'success',
+                'message' => 'Already Subscribed !',
+            ]);
+        }
+
+        $subscribe = new Subscriber();
+        $subscribe->email = $request->email;
+        try {
+            $subscribe->save();
+            return response()->json([
+                'type' => 'success',
+                'message' => 'Successfully Subscribed !.',
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'type' => 'error',
+                'message' => 'Something going wrong. ' . $exception . getMessage(),
+            ]);
+        }
     }
 }
