d433aac81fbb7f482fbd96044c6cfb760d577260
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2021-11-11 15:49:06 +0600

    dashboard page ready

--- app/Http/Controllers/SuperAdmin/DashboardController.php
+++ app/Http/Controllers/SuperAdmin/DashboardController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booster;
+use App\Models\Center;
+use App\Models\PcrTest;
+use App\Models\User;
+use App\Models\Vaccination;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard(){
-        return view('SuperAdmin.dashboard');
+        $users = User::where('status','1')->get();
+        $centers = Center::where('status','1')->get();
+        $pcrTests = PcrTest::all();
+        $vaccinations = Vaccination::all();
+        $boosters = Booster::all();
+        return view('SuperAdmin.dashboard', compact('users','centers','pcrTests','vaccinations','boosters'));
     }
 }
--- resources/views/SuperAdmin/dashboard.blade.php
+++ resources/views/SuperAdmin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('SuperAdmin.layouts.master')
 
 @push('title')
-    Super Admin
+Super Admin
 @endpush
 
 @push('css')
@@ -11,35 +11,185 @@
 @section('content')
 <div class="content">
     <div class="container">
-
         <!-- Page-Title -->
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-header-title">
                     <h4 class="pull-left page-title">Dashboard</h4>
                     <ol class="breadcrumb pull-right">
-                        <li><a href="#">Xadmino</a></li>
+                        <li><a href="{{ route('superAdmin.dashboard') }}">Super Admin Panel</a></li>
                         <li class="active">Dashboard</li>
                     </ol>
                     <div class="clearfix"></div>
                 </div>
             </div>
         </div>
-
         <div class="row">
             <div class="col-sm-6 col-lg-3">
                 <div class="panel panel-primary text-center">
                     <div class="panel-heading">
-                        <h4 class="panel-title">Total Subscription</h4>
+                        <h4 class="panel-title">Total User</h4>
                     </div>
                     <div class="panel-body">
-                        <h3 class=""><b>2568</b></h3>
-                        <p class="text-muted"><b>48%</b> From Last 24 Hours</p>
+                        <h3 class=""><b>{{ $users->where('user_type','user')->count() }}</b></h3>
                     </div>
                 </div>
             </div>
-
             <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-primary text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Total Administrator</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $users->where('user_type','administrator')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-primary text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Total Volunteer</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $users->where('user_type','volunteer')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-primary text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Total Receptionist</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $users->where('user_type','receptionist')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-primary text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Total Pathologist</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $users->where('user_type','pathologist')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-success text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Total Medical Center</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $centers->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-dark text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Premium PCR </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $pcrTests->where('registration_type','premium')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-dark text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Registered PCR </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $pcrTests->where('registration_type','normal')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-dark text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Positive PCR </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $pcrTests->where('pcr_result','positive')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-dark text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Negative PCR </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $pcrTests->where('pcr_result','negative')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-info text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Premium Vaccination </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $vaccinations->where('registration_type','premium')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-info text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Registered Vaccination </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $vaccinations->where('registration_type','normal')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-info text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">1st Dose Taken </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $vaccinations->where('date_of_first_dose','!=',null)->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-info text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">2nd Dose Taken </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $vaccinations->where('date_of_second_dose','!=',null)->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-warning text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Premium Booster </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $boosters->where('registration_type','premium')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="panel panel-warning text-center">
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-white">Registered Booster </h4>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class=""><b>{{ $boosters->where('registration_type','normal')->count() }}</b></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            {{-- <div class="col-sm-6 col-lg-3">
                 <div class="panel panel-primary text-center">
                     <div class="panel-heading">
                         <h4 class="panel-title">Order Status</h4>
@@ -49,35 +199,10 @@
                         <p class="text-muted"><b>15%</b> Orders in Last 10 months</p>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="panel panel-primary text-center">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">Unique Visitors</h4>
-                    </div>
-                    <div class="panel-body">
-                        <h3 class=""><b>25487</b></h3>
-                        <p class="text-muted"><b>65%</b> From Last 24 Hours</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="panel panel-primary text-center">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">Monthly Earnings</h4>
-                    </div>
-                    <div class="panel-body">
-                        <h3 class=""><b>$2779.7</b></h3>
-                        <p class="text-muted"><b>31%</b> From Last 1 month</p>
-                    </div>
-                </div>
-            </div>
+            </div> --}}
         </div>
-
         <div class="row">
-            <div class="col-sm-4">
+            {{-- <div class="col-sm-4">
                 <div class="panel panel-primary">
                     <div class="panel-body">
                         <h4 class="m-t-0">Revenue</h4>
@@ -101,68 +226,13 @@
                         <div id="sparkline1" style="margin: 0 -21px -22px -22px;"></div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <div class="panel-body">
-                        <h4 class="m-t-0">Email Sent</h4>
-                        <ul class="list-inline m-t-30 widget-chart text-center">
-                            <li>
-                                <i class="mdi mdi-arrow-up-bold-circle text-success"></i>
-                                <h4 class=""><b>3654</b></h4>
-                                <h5 class="text-muted m-b-0">Marketplace</h5>
-                            </li>
-                            <li>
-                                <i class="mdi mdi-arrow-down-bold-circle text-danger"></i>
-                                <h4 class=""><b>954</b></h4>
-                                <h5 class="text-muted m-b-0">Last week</h5>
-                            </li>
-                            <li>
-                                <i class="mdi mdi-arrow-up-bold-circle text-success"></i>
-                                <h4 class=""><b>8462</b></h4>
-                                <h5 class="text-muted m-b-0">Last Month</h5>
-                            </li>
-                        </ul>
-                        <div id="sparkline2" style="margin: 0 -21px -22px -22px;"></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <div class="panel-body">
-                        <h4 class="m-t-0">Monthly Subscriptions</h4>
-                        <ul class="list-inline m-t-30 widget-chart text-center">
-                            <li>
-                                <i class="mdi mdi-arrow-up-bold-circle text-success"></i>
-                                <h4 class=""><b>3256</b></h4>
-                                <h5 class="text-muted m-b-0">Marketplace</h5>
-                            </li>
-                            <li>
-                                <i class="mdi mdi-arrow-down-bold-circle text-danger"></i>
-                                <h4 class=""><b>785</b></h4>
-                                <h5 class="text-muted m-b-0">Last week</h5>
-                            </li>
-                            <li>
-                                <i class="mdi mdi-arrow-up-bold-circle text-success"></i>
-                                <h4 class=""><b>1546</b></h4>
-                                <h5 class="text-muted m-b-0">Last Month</h5>
-                            </li>
-                        </ul>
-                        <div id="sparkline3" style="margin: 0 -21px -22px -22px;"></div>
-                    </div>
-                </div>
-            </div>
+            </div> --}}
         </div>
-
-
     </div> <!-- container -->
-
 </div> <!-- content -->
 @endsection
 
 @push('script')
-    {{-- <script src="{{ asset('assets/super-admin/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script> --}}
-    <script src="{{ asset('assets/super-admin/pages/dashborad.js') }}"></script>
+{{-- <script src="{{ asset('assets/super-admin/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script> --}}
+<script src="{{ asset('assets/super-admin/pages/dashborad.js') }}"></script>
 @endpush
