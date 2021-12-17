98fc9b4f7f06a297fa3d34f67c70f36b3fd70ff9
Jahid Hassan <32175937+Jahid1499@users.noreply.github.com>
2021-12-18 02:52:08 +0600

    external vacination information upload without document

--- app/Http/Controllers/Api/ServiceRegistrationController.php
+++ app/Http/Controllers/Api/ServiceRegistrationController.php
@@ -18,6 +18,7 @@ use function MongoDB\BSON\toJSON;
 
 class ServiceRegistrationController extends Controller
 {
+
     public function vaccinationCenterSelect(Request $request)
     {
         $userArray = json_decode($request->getContent(), true);
@@ -125,6 +126,71 @@ class ServiceRegistrationController extends Controller
                 ]);
             }
         }
+    }
+
+    public function externalVaccination(Request $request)
+    {
+        $userArray = json_decode($request->getContent(), true);
+        $phone = $userArray['phone'];
+        $vaccineName = $userArray['vaccineName'];
+        $center = $userArray['center'];
+        $firstDose = $userArray['firstDose'];
+        $secondDose = $userArray['secondDose'];
+        $description = $userArray['description'];
+        $document = $userArray['document'];
+        $centerLocation = $userArray['centerLocation'];
+
+
+        $user = User::where('phone', $phone)->select(['id'])->first();
+        $existVaccination = Vaccination::where('user_id', $user->id)->first();
+
+        if ($existVaccination)
+        {
+            return response()->json([
+                "message"=>"You are already registered for Vaccination. Thank you",
+                "status"=>"2",
+            ]);
+        }
+
+        $vaccine = new Vaccination();
+
+        $vaccine->user_id = $user->id;
+        $vaccine->name_of_vaccine = $vaccineName;
+        $vaccine->date_of_first_dose = Carbon::parse($firstDose);
+        $vaccine->date_of_second_dose = Carbon::parse($secondDose);
+        $vaccine->antibody_last_date = Carbon::parse($secondDose)->addDays(30);
+        $vaccine->registration_type = "normal";
+        $vaccine->status = "1";
+
+        $vaccine->center_name = $center;
+        $vaccine->center_location = $centerLocation;
+        $vaccine->document = $document;
+        $vaccine->description = $description;
+        $vaccine->center_type = "external";
+
+        try {
+            if ($vaccine->save())
+            {
+                return response()->json([
+                    "message"=>"You are successfully submit your information",
+                    "status"=>"1",
+                ]);
+            }else{
+                return response()->json([
+                    "message"=>"Something wrong",
+                    "status"=>"0",
+                ]);
+            }
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                "message"=>'Something went wrong. ' . $exception->getMessage(),
+                "status"=>"0",
+            ]);
+        }
+
+
+
     }
 
     public function prcRegistration(Request $request)
