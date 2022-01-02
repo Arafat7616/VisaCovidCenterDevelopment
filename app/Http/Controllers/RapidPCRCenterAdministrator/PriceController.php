e9faae4234646295d714558825a61ba539b75167
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-01-02 17:13:22 +0600

    Update PriceController.php

--- app/Http/Controllers/RapidPCRCenterAdministrator/PriceController.php
+++ app/Http/Controllers/RapidPCRCenterAdministrator/PriceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\RapidPCRCenterAdministrator;
 
 use App\Http\Controllers\Controller;
 use App\Models\Price;
-use App\Models\RapidPCRCenter;
 use App\Models\RapidPCRPrice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
