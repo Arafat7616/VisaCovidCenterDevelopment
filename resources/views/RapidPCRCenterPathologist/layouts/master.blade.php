23b0a54ac609c9d961605355c5dcc99644c7fab1
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-01-05 13:59:46 +0600

    resource file copied from pathologist

---
+++ resources/views/RapidPCRCenterPathologist/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @include('Pathologist.layouts.includes.head')
+</head>
+
+
+<body class="fixed-left">
+    @if(Auth::check())
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+    @endif
+
+    @include('Pathologist.layouts.includes.navbar')
+    <div>
+        @yield('content')
+    </div>
+
+    @include('Pathologist.layouts.includes.footer')
+
+    @include('Pathologist.layouts.includes.foot')
+</body>
+
+</html>
