2ad70db29bc3af311e01fca1237fe20aede89696
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-01-13 12:58:09 +0600

    code copied from administrator for rtpcr-administrator

--- app/Http/Controllers/RapidPCRCenterAdministrator/DashboardController.php
+++ app/Http/Controllers/RapidPCRCenterAdministrator/DashboardController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\RapidPCRCenterAdministrator;
 
 use App\Http\Controllers\Controller;
+use App\Models\CenterArea;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class DashboardController extends Controller
 {
@@ -17,6 +19,28 @@ class DashboardController extends Controller
     public function profile(){
         $user = Auth::user();
         return view('RapidPCRCenterAdministrator.profile.index', compact('user'));
+    }
+
+    public function centerSpace(){
+        $center = Auth::user()->center;
+        $centerAreas = CenterArea::where('status', 1)->get();
+        return view('RapidPCRCenterAdministrator.profile.center-space', compact('center','centerAreas'));
+    }
+
+    public function updateCenterSpace(Request $request){
+        $request->validate([
+           'space' =>'required'
+        ]);
+
+        $center = Auth::user()->center;
+        // dd($center);
+        $center->center_area_id = $request->space;
+        $center->status = 0;
+        $center->save();
+
+        Session::flash('success', 'Updated successfully!');
+
+        return back();
     }
 
 }
